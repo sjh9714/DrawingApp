7cf53509e9495de376ebd7c1fef2082487a6a2da
sjh9714 <ymh9714@naver.com>
2021-07-29 01:45:16 +0900

    DrawingApp(Kotlin)

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -15,77 +16,104 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
 
+    // 객체 생성 시 호출되어 실행
     init {
         setUpdrawing()
     }
 
     private fun setUpdrawing() {
-        mDrawPaint = Paint()
+        mDrawPaint = Paint() // 페인트 객체 생성
         mDrawPath = CustomPath(color, mBrushSize)
-        mDrawPaint!!.color = color
+        mDrawPaint!!.color = color // Color.BLACK 설정
         mDrawPaint!!.style = Paint.Style.STROKE
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND // 끝이 둥근 모양
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        mBrushSize = 20.toFloat() // 선 굵기 20
     }
 
-    //화면의 사이즈가 변하거나 초기 액티비티가 생성될 때 호출되는 메소드
+    // 화면의 사이즈가 변하거나 초기 액티비티가 생성될 때 호출되는 메소드
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888) //비트맵 생성
-        canvas = Canvas(mCanvasBitmap!!) //캔버스 생성
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888) // 비트맵 생성
+        canvas = Canvas(mCanvasBitmap!!) // 캔버스 생성
     }
 
-    //화면을 그려주는 메소드
+    // 화면을 그려주는 메소드
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        //mCanvasBitmap을 불러와 (0, 0) 위치에 그림
+        // mCanvasBitmap을 불러와 (0, 0) 위치에 그림
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        // 이전의 Path들이 사라지지 않게함
+        for (path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if (!mDrawPath!!.isEmpty) {
-            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness //브러시 두께만큼 경로를 그림
-            mDrawPaint!!.color = mDrawPath!!.color //지정된 색으로 경로를 그림
-            canvas.drawPath(mDrawPath!!, mDrawPaint!!) //지정된 경로를 그림
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness // 브러시 두께만큼 Path를 그림
+            mDrawPaint!!.color = mDrawPath!!.color // 지정된 색으로 Path를 그림
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!) // 지정된 Path를 그림
         }
     }
 
-    //화면 터치 이벤트
+    // 화면 터치 이벤트
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        //화면 터치 x,y 값
+        // 화면 터치 x,y 값
         val touchX = event?.x
         val touchY = event?.y
 
         when (event?.action) {
-            //화면 터치
+            // 화면 터치
             MotionEvent.ACTION_DOWN -> {
-                mDrawPath!!.color = color //색상
-                mDrawPath!!.brushThickness = mBrushSize //굵기
+                mDrawPath!!.color = color // 색상
+                mDrawPath!!.brushThickness = mBrushSize // 굵기
 
                 mDrawPath!!.reset()
                 if (touchX != null) {
                     if (touchY != null) {
-                        mDrawPath!!.moveTo(touchX!!, touchY!!) //위치만 이동
+                        mDrawPath!!.moveTo(touchX!!, touchY!!) // 위치만 이동
                     }
                 }
             }
-            //화면 드래그
+            // 화면 드래그
             MotionEvent.ACTION_MOVE -> {
                 if (touchX != null) {
                     if (touchY != null) {
-                        mDrawPath!!.lineTo(touchX!!, touchY!!) //선을 그림
+                        mDrawPath!!.lineTo(touchX!!, touchY!!) // 선을 그림
                     }
                 }
             }
-            //화면 터치 해제
+            // 화면 터치 해제
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!) // Path 저장
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
         }
-        invalidate() //화면 다시 그리기
+        invalidate() // 화면 다시 그리기
 
         return true
+    }
+
+    // 브러쉬 크기 변경
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    // 색상 변경
+    fun setColor(newColor: String) {
+        color = Color.parseColor(newColor)
+        mDrawPaint!!.color = color
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,11 +1,99 @@
 package com.example.drawingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import com.example.drawingapp.databinding.ActivityMainBinding
+import com.example.drawingapp.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 익스텐션 대신 뷰바인딩 사용
+    private lateinit var binding: ActivityMainBinding
+
+    // 페인트 색상 변수
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // 액티비티 뷰바인딩 기본적 양식
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 초기 브러쉬 사이즈 10
+        binding.drawingView.setSizeForBrush(10.toFloat())
+
+        // llPaintColors의 2번째 색상인 검정색 사용
+        mImageButtonCurrentPaint = binding.llPaintColors[1] as ImageButton
+        // 페인트 색상 클릭 시 테두리 변경
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
+
+        // ibBrush 버튼 클릭 시
+        binding.ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    // 브러쉬 사이즈 선택 이벤트
+    private fun showBrushSizeChooserDialog() {
+        // 뷰바인딩, 기본 다이얼로그 설정
+        var bindingBrush = DialogBrushSizeBinding.inflate(layoutInflater)
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(bindingBrush.root)
+        brushDialog.setTitle("Brush size: ")
+
+        // 스몰 버튼 클릭
+        val smallBtn = bindingBrush.ibSmallBrush
+        smallBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            // 다이얼로그 끄기기
+           brushDialog.dismiss()
+        }
+
+        // 미디엄 버튼 클릭
+        val mediumBtn = bindingBrush.ibMediumBrush
+        mediumBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        // 라지 버튼 클릭
+        val largeBtn = bindingBrush.ibLargeBrush
+        largeBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        // 다이얼로그 실행
+        brushDialog.show()
+    }
+
+    // 페인트 클릭 이벤트
+    fun paintClicked(view: View) {
+        // 선택한 색상과 이전의 색상이 같지 않을 때
+        if (view !== mImageButtonCurrentPaint) {
+            // 이미지버튼을 view로 받아옴
+            val imageButton = view as ImageButton
+            // xml에 입력해둔 태그를 받아옴
+            val colorTag = imageButton.tag.toString()
+            // 태그로 색상 변경
+            binding.drawingView.setColor(colorTag)
+            // 바뀐 색상의 테두리 변경
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+            // 이전 색상의 테두리 변경
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            // 색상값을 바뀐 view로 변경
+            mImageButtonCurrentPaint = view
+        }
     }
 }
